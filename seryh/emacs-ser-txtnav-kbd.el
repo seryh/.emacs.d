1882b3d725b0866afd601b76afb4ff6919e7284d
seryh <o.seryh@gmail.com>
2016-09-11 13:47:02 +0700

    kdb

--- seryh/emacs-ser-txtnav-kbd.el
+++ seryh/emacs-ser-txtnav-kbd.el
@@ -1,6 +1,5 @@
 ;; Крестовина
 ;;
-
 ;; unsets
 (global-unset-key (kbd "M-SPC"))
 (global-unset-key (kbd "M-m"))
@@ -20,57 +19,39 @@
 (global-unset-key (kbd "M-k"))
 (global-unset-key (kbd "M-i"))
 
-
-
 ;; Вверх
 (global-set-key (kbd "M-i") 'previous-line)
-
 ;; Вниз
 (global-set-key (kbd "M-k") 'next-line)
-
 ;; Влево
 (global-set-key (kbd "M-j") 'backward-char)
-
 ;; Вправо
 (global-set-key (kbd "M-l") 'forward-char)
-
 ;; Page Up
 (global-set-key (kbd "M-I") 'scroll-down-command)
-
 ;; Page Down
 (global-set-key (kbd "M-K") 'scroll-up-command)
-
 ;; Forward word
 (global-set-key (kbd "M-o") 'forward-word)
-
 ;; Backward word
 (global-set-key (kbd "M-u") 'backward-word)
-
 ;; Beginnning of line
 (global-set-key (kbd "M-h") 'move-end-of-line)
-
 ;; End of line
 (global-set-key (kbd "M-H") 'move-beginning-of-line)
-
 ;; End of buffer
 (global-set-key (kbd "M-L") 'end-of-buffer)
-
 ;; Beginning of buffer
 (global-set-key (kbd "M-J") 'beginning-of-buffer)
 
-
 ;; Редактирование
 ;;
-
 ;; Delete
 (global-set-key (kbd "M-f") 'delete-forward-char)
-
 ;; Backspace
 (global-set-key (kbd "M-d") 'delete-backward-char)
-
 ;; Enter
 (global-set-key (kbd "M-n") 'reindent-then-newline-and-indent)
 (global-set-key (kbd "M-m") 'reindent-then-newline-and-indent)
-
 ;; Выделение
 (global-set-key (kbd "M-SPC") 'set-mark-command)
