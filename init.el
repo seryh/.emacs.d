56b2494d3868f2335f2ea828a86cafc8dd56dc8b
seryh <o.seryh@gmail.com>
2016-09-18 00:56:48 +0700

    markdown mode

--- init.el
+++ init.el
@@ -148,9 +148,15 @@
 (load (ser/get-config-dir "seryh/emacs-ser-txtnav-kbd.el"))
 (load (ser/get-config-dir "seryh/turn-off-messaging.el"))
 
-
-
 (require 'gulpjs)
+
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (setq auto-mode-alist
       (append '(("\\.scss$". sass-mode)
