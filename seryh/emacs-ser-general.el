82002ae39e3805d83f670278647f7eb9ebd86661
seryh <o.seryh@gmail.com>
2016-09-24 12:51:07 +0700

    os x font fix

--- seryh/emacs-ser-general.el
+++ seryh/emacs-ser-general.el
@@ -124,6 +124,26 @@
 ;;(show-paren-mode t) ;; включить выделение выражений между {},[],()
 ;;(setq show-paren-style 'expression) ;; выделить цветом выражения между {},[],()
 
+(when (eq system-type 'darwin)
+
+  ;; default Latin font (e.g. Consolas)
+  (set-face-attribute 'default nil :family "Consolas")
+
+  ;; default font size (point * 10)
+  ;;
+  ;; WARNING!  Depending on the default font,
+  ;; if the size is not supported very well, the frame will be clipped
+  ;; so that the beginning of the buffer may not be visible correctly. 
+  (set-face-attribute 'default nil :height 155)
+
+  ;; use specific font for Korean charset.
+  ;; if you want to use different font size for specific charset,
+  ;; add :size POINT-SIZE in the font-spec.
+  (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
+
+  ;; you may want to add different for other charset in this way.
+  )
+
 (setq which-key-idle-delay 1.0)
 (setq which-key-separator " → " )
 
