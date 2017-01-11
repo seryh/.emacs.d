437bddcd586e938b917234a0378d0ac6a438eb53
o.seryh <o.seryh@gmail.com>
2017-01-11 15:31:13 +0700

    helm config

--- init.el
+++ init.el
@@ -125,7 +125,35 @@
 ;;(require 'cider)
 
 ;; --------------------------------------------------------[ helm ]
-;;(helm-autoresize-mode t)
+(require 'helm)
+(require 'helm-config)
+
+(helm-autoresize-mode t)
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+
+(helm-mode 1)
 
 ;; ------------------------------------------------------- [ bookmark ]
 (require 'bookmark+)
