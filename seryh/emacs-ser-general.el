6f587e1bc7da44aa4916ca8ae1ed075893e88c79
o.seryh <o.seryh@gmail.com>
2017-05-19 14:06:57 +0700

    add fn

--- seryh/emacs-ser-general.el
+++ seryh/emacs-ser-general.el
@@ -219,3 +219,18 @@
 
 (add-hook 'after-init-hook (lambda () (set-cursor-color "magenta")))
 ;;(add-hook 'input-method-activate-hook (lambda () (set-cursor-color "magenta")))
+
+;; -------------------------------- [ transparency ]
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (eql (cond ((numberp alpha) alpha)
+                    ((numberp (cdr alpha)) (cdr alpha))
+                    ;; Also handle undocumented (<active> <inactive>) form.
+                    ((numberp (cadr alpha)) (cadr alpha)))
+              100)
+         '(85 . 50) '(100 . 100)))))
+
+(global-set-key (kbd "C-c t") 'toggle-transparency)
