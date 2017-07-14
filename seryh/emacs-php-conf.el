00dadbd9946042c380c17ee59a40039a75cf5d9d
Oleg Seryh <o.seryh@gmail.com>
2017-07-14 14:28:19 +0700

    add php mode

---
+++ seryh/emacs-php-conf.el
@@ -0,0 +1,16 @@
+(use-package php-mode
+  :commands php-mode
+  :ensure t
+  :mode "\\.php?$"
+  :init
+  (add-hook 'php-mode-hook 'php-enable-psr2-coding-style)
+  (add-hook 'php-mode-hook (lambda ()
+                                 ;;(paredit-mode t)
+                                 (company-mode t)
+                                 (hs-minor-mode t) ;;show/hide block
+                                 (aggressive-indent-mode t)
+                                 ))
+
+  )
+
+(provide 'emacs-php-conf)
